c9b5f4579f21d5a7d2d44c02ba049023ee1c316e
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-20 21:55:17 +0600

    Introduction

--- IntroductionLesson/Program.cs
+++ IntroductionLesson/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IntroductionLesson
@@ -10,6 +13,50 @@ namespace IntroductionLesson
     {
         static void Main(string[] args)
         {
+            using (Socket socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                string ipAddress = "0.0.0.0";
+                //string ipAddress = "127.0.0.1";
+                //string ipAddress = "10.1.4.46";
+                int port = 12345;
+
+                socketServer.Bind(new IPEndPoint(IPAddress.Parse(ipAddress), port));
+                socketServer.Listen(100);
+
+                while (true)
+                {
+                    Socket client = socketServer.Accept();
+
+                    Console.WriteLine("Клинет подключился: ");
+                    Console.WriteLine(client.RemoteEndPoint.ToString());
+
+                    ThreadPool.QueueUserWorkItem(Temp, client);
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Temp(object state)
+        {
+            Socket client = state as Socket;
+            try
+            {
+                byte[] buf = new byte[1024];
+                while (true)
+                {
+                    int recSize = client.Receive(buf);
+                    client.Send(buf, recSize, SocketFlags.None);
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
         }
     }
 }
